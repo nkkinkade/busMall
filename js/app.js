439e302775623ba3f1be0922ed8f01afc2abf9c2
Nathan Kinkade <nkkinkade@gmail.com>
2020-06-29 17:50:15 -0500

    add list, styled page

--- js/app.js
+++ js/app.js
@@ -12,30 +12,34 @@ function Product(name, imageUrl) {
   this.name = name;
   this.imageUrl = imageUrl;
   this.timesClicked = 0;
+  this.timesShown = 0;
   allProducts.push(this);
 }
 
-new Product('bag', 'images/bag.jpg');
-new Product('banana', 'images/banana.jpg');
-new Product('bathroom', 'images/bathroom.jpg');
-new Product('boots', 'images/boots.jpg');
-new Product('breakfast', 'images/breakfast.jpg');
-new Product('bubblegum', 'images/bubblegum.jpg');
-new Product('chair', 'images/chair.jpg');
-new Product('cthulhu', 'images/cthulhu.jpg');
-new Product('dog-duck', 'images/dog-duck.jpg');
-new Product('dragon', 'images/dragon.jpg');
-new Product('pen', 'images/pen.jpg');
-new Product('pet-sweep', 'images/pet-sweep.jpg');
-new Product('scissors', 'images/scissors.jpg');
-new Product('shark', 'images/shark.jpg');
-new Product('sweep', 'images/sweep.png');
-new Product('tauntaun', 'images/tauntaun.jpg');
-new Product('unicorn', 'images/unicorn.jpg');
-new Product('usb', 'images/usb.gif');
-new Product('water-can', 'images/water-can.jpg');
-new Product('wine-glass', 'images/wine-glass.jpg');
+new Product('R2-D2 luggage', 'images/bag.jpg');
+new Product('Banana slicer', 'images/banana.jpg');
+new Product('Bathroom tablet', 'images/bathroom.jpg');
+new Product('Missing toe boots', 'images/boots.jpg');
+new Product('Breakfast maker', 'images/breakfast.jpg');
+new Product('Meatball bubblegum', 'images/bubblegum.jpg');
+new Product('Red chair', 'images/chair.jpg');
+new Product('Lord Cthulhu', 'images/cthulhu.jpg');
+new Product('Duck dog costume', 'images/dog-duck.jpg');
+new Product('Dragon meat', 'images/dragon.jpg');
+new Product('Utensil pen', 'images/pen.jpg');
+new Product('Pet Sweep', 'images/pet-sweep.jpg');
+new Product('Pizza scissors', 'images/scissors.jpg');
+new Product('Plush shark', 'images/shark.jpg');
+new Product('Baby sweeper', 'images/sweep.png');
+new Product('Tauntaun sleeping bag', 'images/tauntaun.jpg');
+new Product('Unicorn meat', 'images/unicorn.jpg');
+new Product('USB tenticle', 'images/usb.gif');
+new Product('Water can', 'images/water-can.jpg');
+new Product('Wine glass', 'images/wine-glass.jpg');
 
+allProducts[0].timesShown = 1;
+allProducts[1].timesShown = 1;
+allProducts[2].timesShown = 1;
 var totalClicks = 0;
 function imageWasClicked(event){
   totalClicks++;
@@ -70,74 +74,25 @@ function imageWasClicked(event){
   productIndex3 = nextproductIndex3;
 
   imageElements[0].src = allProducts[productIndex1].imageUrl;
+  allProducts[productIndex1].timesShown++;
   imageElements[1].src = allProducts[productIndex2].imageUrl;
+  allProducts[productIndex2].timesShown++;
   imageElements[2].src = allProducts[productIndex3].imageUrl;
+  allProducts[productIndex3].timesShown++;
 
-  if(totalClicks >= rounds) {
-    var footerElement = document.getElementsByTagName('footer')[0];
-    if(footerElement.firstElementChild){
-      footerElement.firstElementChild.remove();
+  if(totalClicks === rounds){
+    var resultsList = document.getElementById('list');
+    for(var i =0; i < allProducts.length; i++){
+      var bMallItem = document.createElement('li');
+      bMallItem.textContent = `${allProducts[i].name} was clicked on ${allProducts[i].timesClicked} times and was shown ${allProducts[i].timesShown} times.`;
+      resultsList.appendChild(bMallItem);
     }
-    footerElement.textContent = '1010101110001.';
-  }
-}
-
-for(var i = 0; i < imageElements.length; i++){
-  imageElements[i].addEventListener('click', imageWasClicked);
-}
-
-
-var data = {
-  labels: [],
-  datasets: [
-    {
-      label: 'Times clicked per item',
-      fillColor: '#2E9329',
-      strokeColor: '#31732E',
-      highlightFill:'#72C56E',
-      hightlightStroke: '#31732E',
-      data: []
-    },
-    {
-      label: 'Times Displayed per item',
-      fillColor: '#EDF3F2',
-      strokeColor: '#B3CFCE',
-      highlightFill: '#F5FAF4',
-      hightlightStroke: '#B3CFCE',
-      data: []
+    for(var j = 0; j < imageElements.length; j++){
+      imageElements[j].removeEventListener('click', imageWasClicked);
     }
-  ]
-};
-
-function resetChartData() {
-  for (var j = 0; j < allProducts.length; j++) {
-    data.labels.push('0');
-    data.datasets[0].data.push('0');
-    data.datasets[1].data.push('0');
   }
-}
-resetChartData();
+}//closes the imageWasClicked function
 
-var resultsCanvas = document.getElementById('resultsCanvas').getContext('2d');
-var chartMe = new Chart(resultsCanvas).Bar(data); //eslint-disable-line
-
-var resultButton = document.getElementById('showResults');
-var numResultButtonClicks = 0;
-resultButton.addEventListener('click', handleResultButtonClick);
-
-function handleResultButtonClick(event) {
-  numResultButtonClicks += 1;
-  var results = document.getElementById('resultsSection');
-  results.removeAttribute('hidden');
-
-  allProducts.sort(function (a, b) {return b.numClicks - a.numClicks;});
-  for(var i = 0; i < allProducts.length; i++)
-  {
-    data.labels[i] = allProducts[i].productName;
-    chartMe.datasets[0].bars[i].label = 'Clicked ' + parseInt(allProducts[i].numClicks/allProducts[i].numDisplays*100) + '%';
-    chartMe.datasets[0].bars[i].value = allProducts[i].numClicks;
-    chartMe.datasets[1].bars[i].value = allProducts[i].numDisplays;
-  }
-  chartMe.update();
-  allProducts.sort(function (a, b) {return a.originalIndex - b.originalIndex;});
+for(var j = 0; j < imageElements.length; j++){
+  imageElements[j].addEventListener('click', imageWasClicked);
 }
